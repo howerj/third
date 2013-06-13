ee1bb2f6399c956e6d846f3117a293243e07704f
Richard Howe <howe.r.j.89@gmail.com>
2013-06-13 17:25:22 +0100

    Rearranged some stuff.

--- third.c
+++ third.c
@@ -10,7 +10,7 @@ int m[2500] = { 32 }, L = 1, I, T[500], *S = T, t = 64, w, f;
 
 enum primitives{
   PUSH,COMPILE,RUN,DEFINE,IMMEDIATE,READ,LOAD,STORE,SUBTRACT,
-  MULTIPLY,DIVIDE,LESSZ,EXIT,ECHO,KEY,PICK,FROMR,TOR,BRANCH,NBRANCH,LAST_ENUM
+  MULTIPLY,DIVIDE,LESSZ,EXIT,EMIT,KEY,PICK,FROMR,TOR,BRANCH,NBRANCH,PRINTNUM,LAST_ENUM
 };
 
 void compile_word(int x)
@@ -22,96 +22,6 @@ void compile_word(int x)
         scanf("%s", s + t);
         t += strlen(s + t) + 1;
         return;
-}
-
-void run(int x)
-{
-INNER:
-        switch (m[x++]) {
-        case PUSH:
-                *++S = f;
-                f = m[I++];
-                break;
-        case COMPILE:
-                c=x;
-                break;
-        case RUN:
-                m[++m[1]] = I;
-                I = x;
-                break;
-        case DEFINE:
-                compile_word(1);
-                c=2;
-                break;
-        case IMMEDIATE:
-                *m -= 2;
-                c=2;
-                break;
-        case READ:
-                for (w = scanf("%s", s) < 1 ? exit(0), 0 : L; strcmp(s, &s[m[w + 1]]); w = m[w]) ;
-                if(w-1){
-                  x=w+2;
-                  goto INNER;
-                } else {
-                  c=2; c=atoi(s);
-                }
-                break;
-        case LOAD:
-                f = m[f];
-                break;
-        case STORE:
-                m[f] = *S--;
-                f = *S--;
-                break;
-        case SUBTRACT:
-                f = *S-- - f;
-                break;
-        case MULTIPLY:
-                f *= *S--;
-                break;
-        case DIVIDE:
-                f = *S-- / f;
-                break;
-        case LESSZ:
-                f = 0 > f;
-                break;
-        case EXIT:
-                I = m[m[1]--];
-                break;
-        case ECHO:
-                putchar(f);
-                f = *S--;
-                break;
-        case KEY:
-                *++S = f;
-                f = getchar();
-                break;
-        case PICK:
-                f = S[-f];
-                break;
-        case FROMR:
-                *++S = f;
-                f = m[m[1]--];
-                break;
-        case TOR:
-                m[++m[1]] = f;
-                f = *S--;
-                break;
-        case BRANCH:
-                I+=m[I];
-                break;
-        case NBRANCH:
-                if(f==0){
-                  I+=m[I] ;
-                } else {
-                  I++;
-                } 
-                f = *S--;
-                break;
-        default:
-                fprintf(stderr,"Unknown instruction\n");
-                exit(1);
-        }
 }
 
 void init(void){
@@ -130,9 +40,107 @@ void init(void){
         *m += 512;
 }
 
+void run(void)
+{
+  int x;
+  while(1){
+    x = m[I++];
+    INNER:
+            switch (m[x++]) {
+            case PUSH:
+                    *++S = f;
+                    f = m[I++];
+                    break;
+            case COMPILE:
+                    c=x;
+                    break;
+            case RUN:
+                    m[++m[1]] = I;
+                    I = x;
+                    break;
+            case DEFINE:
+                    compile_word(1);
+                    c=2;
+                    break;
+            case IMMEDIATE:
+                    *m -= 2;
+                    c=2;
+                    break;
+            case READ:
+                    for (w = scanf("%s", s) < 1 ? exit(0), 0 : L; strcmp(s, &s[m[w + 1]]); w = m[w]) ;
+                    if(w-1){
+                      x=w+2;
+                      goto INNER;
+                    } else {
+                      c=2; c=atoi(s);
+                    }
+                    break;
+            case LOAD:
+                    f = m[f];
+                    break;
+            case STORE:
+                    m[f] = *S--;
+                    f = *S--;
+                    break;
+            case SUBTRACT:
+                    f = *S-- - f;
+                    break;
+            case MULTIPLY:
+                    f *= *S--;
+                    break;
+            case DIVIDE:
+                    f = *S-- / f;
+                    break;
+            case LESSZ:
+                    f = 0 > f;
+                    break;
+            case EXIT:
+                    I = m[m[1]--];
+                    break;
+            case EMIT:
+                    putchar(f);
+                    f = *S--;
+                    break;
+            case KEY:
+                    *++S = f;
+                    f = getchar();
+                    break;
+            case PICK:
+                    f = S[-f];
+                    break;
+            case FROMR:
+                    *++S = f;
+                    f = m[m[1]--];
+                    break;
+            case TOR:
+                    m[++m[1]] = f;
+                    f = *S--;
+                    break;
+            case BRANCH:
+                    I+=m[I];
+                    break;
+            case NBRANCH:
+                    if(f==0){
+                      I+=m[I] ;
+                    } else {
+                      I++;
+                    } 
+                    f = *S--;
+                    break;
+            case PRINTNUM:
+                    printf("%d",f);
+                    f = *S--;
+                    break;
+            default:
+                    fprintf(stderr,"Unknown instruction\n");
+                    exit(1);
+            }
+  }
+}
+
 int main(void)
 {
         init();
-        for (;; run(m[I++])) ;
+        run();
         return 0;
 }
--- third.fs
+++ third.fs
@@ -1,4 +1,4 @@
-: immediate _read @ ! - * / <0 exit emit key _pick r> >r branch notbranch
+: immediate _read @ ! - * / <0 exit emit key _pick r> >r branch notbranch printnum
 : r 1 exit
 
 : ] r @ 1 - r ! _read ]
@@ -10,43 +10,26 @@ _main
 : _y 4 @ exit
 : _x! 3 ! exit
 : _y! 4 ! exit
-
-
 : swap _x! _y! _x _y exit
-
-: + 0 swap - - exit
-
 : dup _x! _x _x exit
-
+: over _x! _y! _y _x _y ;
+: + 0 swap - - exit
 : inc dup @ 1 + swap ! exit
-
 : h 0 exit
-
 : , h @ ! h inc exit
-
 : ' r @ @ dup 1 + r @ ! @ exit
-
 : ; immediate ' exit , exit
-
 : drop 0 * + ;
-
-: dec dup @ 1 - swap ! ;
-
 : tail r> r> drop >r ;
-
 : minus 0 swap - ;
-
 : bnot 1 swap - ;
-
 : < - <0 ;
-
+: > swap < ;
+: <= 1 + < ;
+: >= swap <= ;
 : logical dup 0 < swap minus 0 < + ;
-
 : not logical bnot ;
-
 : = - not ;
-
-
 : here h @ ;
 
 : if immediate ' notbranch , here 0 , ;
@@ -78,38 +61,10 @@ _main
   0 ,            ( compile a dummy offset for branch )
   swap            ( bring old backpatch address to top )
   dup here swap -    ( calculate the offset from old address )
-  swap !        ( put the address on top and s>re it )
-;
-
-: over _x! _y! _y _x _y ;
-
-: add
-  _x!            ( save the pointer in a temp variable )
-  _x @            ( get the value pointed to )
-  +            ( add the incremement from on top of the stack )
-  _x !            ( and save it )
-;
-
-: allot    h add ;
-
-
-: mod _x! _y!        ( get x then y off of stack )
-  _y
-  _y _x / _x *        ( y - y / x * x )
-  -
+  swap !        ( put the address on top and store it )
 ;
 
 : cr '\n' emit exit
-
-: printnum
-  dup
-  10 mod '0' +
-  swap 10 / dup
-  if
-    printnum 0
-  then
-  drop emit
-;
 
 : .
   dup 0 <
@@ -120,8 +75,6 @@ _main
   'space' emit
 ;
 
-
-: debugprint dup . cr ;
 
 ( the following routine takes a pointer to a string, and prints it,
   except for the trailing quote.  returns a pointer to the next word
@@ -162,44 +115,7 @@ _main
   find-"
 ;
 
-: do immediate
-  ' swap ,        ( compile 'swap' to swap the limit and start )
-  ' >r ,        ( compile to push the limit onto the return stack )
-  ' >r ,        ( compile to push the start on the return stack )
-  here            ( save this address so we can branch back to it )
-;
-
-: i r @ 1 - @ ;
-: j r @ 3 - @ ;
-
-: > swap < ;
-: <= 1 + < ;
-: >= swap <= ;
-
-: inci 
-  r @ 1 -     ( get the pointer to i )
-  inc        ( add one to it )
-  r @ 1 - @     ( find the value again )
-  r @ 2 - @    ( find the limit value )
-  <
-  if
-    r @ @ @ r @ @ + r @ ! exit        ( branch )
-  then
-  r> 1 +
-  r> drop
-  r> drop
-  >r
-;
       
-: loop immediate ' inci , here - , ;
-
-: loopexit
-
-  r> drop        ( pop off our return address )
-  r> drop        ( pop off i )
-  r> drop        ( pop off the limit of i )
-;            ( and return to the caller's caller routine )
-
 : execute
   8 !
   ' exit 9 !
@@ -215,7 +131,7 @@ fix-::
 : : immediate :: ] ;
 
 : command
-  here 5 !        ( s>re dict pointer in temp variable )
+  here 5 !        ( store dict pointer in temp variable )
   _read            ( compile a word )
             ( if we get control back: )
   here 5 @
@@ -234,34 +150,6 @@ fix-::
   tail command
 ;
 
-: make-immediate    ( make a word just compiled immediate )
-  here 1 -        ( back up a word in the dictionary )
-  dup dup        ( save the pointer to here )
-  h !            ( s>re as the current dictionary pointer )
-  @            ( get the run-time code pointer )
-  swap            ( get the dict pointer again )
-  1 -            ( point to the compile-time code pointer )
-  !            ( write run-time code pointer on compile-time pointer )
-;
-
-: <build immediate
-  make-immediate    ( make the word compiled so far immediate )
-  ' :: ,        ( compile '::', so we read next word )
-  2 ,            ( compile 'pushint' )
-  here 0 ,        ( write out a 0 but save address for does> )
-  ' , ,            ( compile a push that address onto dictionary )
-;
-
-: does> immediate
-  ' command ,        ( jump back into command mode at runtime )
-  here swap !        ( backpatch the build> to point to here )
-  2 ,            ( compile run-code primitive so we look like a word )
-  ' r> ,        ( compile r>, which leaves var address on stack )
-;
-
-: var <build , does> ;
-: constant <build , does> @ ;
-: array <build allot does> + ;
 
 : [ immediate command ;
 : _welcome " Welcome to THIRD.
